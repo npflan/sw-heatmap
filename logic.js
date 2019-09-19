3a8ad94760d2341d30993e934e661378af86a7f4
bigphattoby <twabias@gmail.com>
2019-09-19 20:48:41 +0200

    using 34 to support short rows in hall C

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
 function changeColor(switchName, switchNum, Color) {
     if (switchNum === 1) {
-        $.merge($('#' + switchName + '01').parent().children(),$('#'+ switchName + '40').parent().children()).filter((idx, seat) => { let i = parseInt(seat.id.substr(2)); return i < 11 || (i > 20 && i < 31) }).each((idx, seat) => { $(seat).children('Rect').css('fill', Color);});
+        $.merge($('#' + switchName + '01').parent().children(),$('#'+ switchName + '34').parent().children()).filter((idx, seat) => { let i = parseInt(seat.id.substr(2)); return i < 11 || (i > 20 && i < 31) }).each((idx, seat) => { $(seat).children('Rect').css('fill', Color);});
     } else {
-        $.merge($('#' + switchName + '01').parent().children(),$('#'+ switchName +'40').parent().children()).filter((idx, seat) => { let i = parseInt(seat.id.substr(2)); return i > 30 || (i > 10 && i < 21) }).each((idx, seat) => { $(seat).children('Rect').css('fill', Color);});
+        $.merge($('#' + switchName + '01').parent().children(),$('#'+ switchName +'34').parent().children()).filter((idx, seat) => { let i = parseInt(seat.id.substr(2)); return i > 30 || (i > 10 && i < 21) }).each((idx, seat) => { $(seat).children('Rect').css('fill', Color);});
     }
     if (switchNum === 0) {
         $.merge($('#' + switchName + '01').parent().children(),$('#'+ switchName +'30').parent().children()).filter((idx, seat) => { let i = parseInt(seat.id.substr(2)); return i < 31 }).each((idx, seat) => { $(seat).children('Rect').css('fill', Color);});
@@ -14,5 +14,5 @@ SwitchNum:
 
 */
 $(document).ready(function(){
-changeColor('XE', 2, 'red')
+changeColor('YH', 1, 'red')
 });
