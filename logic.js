30a2cbf8da89c985e302aacaedf170b3f6ae8762
bigphattoby <twabias@gmail.com>
2019-09-27 21:43:44 +0200

    inital test successfull

--- logic.js
+++ logic.js
@@ -1,35 +1,42 @@
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
+}
+
 function changeColor(switchName, switchNum, Color) {
+  if (
+    (switchName >= "FA" && switchName <= "FS") ||
+    (switchName >= "GA" && switchName <= "GS")
+  ) {
+    $.merge(
+      $("#" + switchName + "01")
+        .parent()
+        .children(),
+      $("#" + switchName + "17")
+        .parent()
+        .children()
+    ).each((idx, seat) => {
+      $(seat)
+        .children("Rect")
+        .css("fill", Color);
+    });
+  } else if (switchName >= "CA" && switchName <= "CG") {
+    $.merge(
+      $("#" + switchName + "01")
+        .parent()
+        .children(),
+      $("#" + switchName + "30")
+        .parent()
+        .children()
+    ).each((idx, seat) => {
+      $(seat)
+        .children("Rect")
+        .css("fill", Color);
+    });
+  }
   if (switchNum === 1) {
-    if (
-      (switchName >= "FA" && switchName <= "FS") ||
-      (switchName >= "GA" && switchName <= "GS")
-    ) {
-      $.merge(
-        $("#" + switchName + "01")
-          .parent()
-          .children(),
-        $("#" + switchName + "17")
-          .parent()
-          .children()
-      ).each((idx, seat) => {
-        $(seat)
-          .children("Rect")
-          .css("fill", Color);
-      });
-    } else if (switchName >= "CA" && switchName <= "CG") {
-      $.merge(
-        $("#" + switchName + "01")
-          .parent()
-          .children(),
-        $("#" + switchName + "30")
-          .parent()
-          .children()
-      ).each((idx, seat) => {
-        $(seat)
-          .children("Rect")
-          .css("fill", Color);
-      });
-    }
     $.merge(
       $("#" + switchName + "01")
         .parent()
@@ -69,5 +76,13 @@ function changeColor(switchName, switchNum, Color) {
 }
 
 $(document).ready(function() {
-  changeColor("FQ", 1, "red");
+  jData = $.parseJSON(httpGet("https://09177742.ngrok.io"));
+  console.log(jData);
+  //console.log(joebob);
+  //var jData = $.parseJSON(
+  //  '[{"name":"AA", "num":1},{"name":"AB", "num":2},{"name":"AC", "num":1},{"name":"AC", "num":2},{"name":"FA","num":1}]'
+  //);
+  $.each(jData, function(i, item) {
+    changeColor(jData[i].name, jData[i].num, "red");
+  });
 });
